43b38c36a98388a13de1bcb60f4678677cb9d9e1
kimseung12 <tmddn113@gmail.com>
2023-10-25 19:28:23 +0900

    2023-10-25

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -9,6 +9,8 @@ import { BiCommentDots } from "react-icons/bi";
 const Video = () => {
     const { videoId } = useParams();
     const [videoDetail, setVideoDetail] = useState(null);
+    const [videoComments, setVideoComments] = useState([]);
+    const [showAllComments, setShowAllComments] = useState(false); // 추가: 댓글을 모두 보이게 할지 여부
 
     useEffect(() => {
         fetchFromAPI(`videos?part=snippet,statistics&id=${videoId}`)
@@ -16,7 +18,18 @@ const Video = () => {
                 setVideoDetail(data.items[0]);
                 console.log(data);
             });
+        fetchFromAPI(`commentThreads?key=36c0488b9bmsha694c50fc1a58dap12df45jsn98128f857dfb&videoId=${videoId}&part=snippet`)
+            .then((data) => {
+                const comments = data.items.slice(0, 20);
+                console.log(comments);
+                setVideoComments(comments);
+            });
     }, [videoId]);
+
+     // "접기" 버튼 클릭 시 댓글 표시 여부를 토글
+     const toggleComments = () => {
+        setShowAllComments(!showAllComments);
+    };
 
     return (
         <section id='videoViewPage'>
@@ -51,6 +64,16 @@ const Video = () => {
                         </div>
                     </div>
                     <div className='video__desc'>{videoDetail.snippet.description}</div>
+                    <div className="video__comments">
+                        {videoComments.map((comment, index) => (
+                            <div key={index} className={`comment ${!showAllComments && index >= 10 ? 'hidden' : ''}`}>
+                                <p>💭 {comment.snippet.topLevelComment.snippet.authorDisplayName} : {comment.snippet.topLevelComment.snippet.textOriginal}</p>
+                            </div>
+                        ))}
+                        <button onClick={toggleComments}>
+                            {showAllComments ? '접기' : '더 보기'}
+                        </button>
+                    </div>
                 </div>
             )}
         </section>
